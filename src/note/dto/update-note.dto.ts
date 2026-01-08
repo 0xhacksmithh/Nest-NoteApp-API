468e5973c738a2c05dbec47257475ac82ebf33db
sathya <leosathya10@gmail.com>
2026-01-08 14:17:41 +0530

    add Api Docs With Swagger

--- src/note/dto/update-note.dto.ts
+++ src/note/dto/update-note.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger'; // for swagger use
 import { CreateNoteDto } from './create-note.dto';
 
 export class UpdateNoteDto extends PartialType(CreateNoteDto) {}
